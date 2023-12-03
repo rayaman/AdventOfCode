411ff37e49ed8bdc92295d6f39c621700461c1af
Ryan Ward <rayaman99@gmail.com>
2023-12-03 12:33:04 -0500

    Finished Day 3

--- Day 3/main.lua
+++ Day 3/main.lua
@@ -14,14 +14,8 @@ local content = file:read("a*")
 -- .664.598..
 -- ]]
 
-local lines = {}
-for line in content:gmatch("(.-)\n") do
-    table.insert(lines,line)
-end
-
-local maxX = #content:match("(.-)\n")
-local _, maxY = content:gsub("(\n)","")
-print(maxX, maxY)
+local sum = 0
+local maxX = #content:match("(.-)\n") + 1
 
 local function find(str, substr, callback, init)
     init = init or 1
@@ -31,68 +25,125 @@ local function find(str, substr, callback, init)
         callback(str, first, last)
         return find(str, substr, callback, last+1)
     end
- end
-
-local function getCords(grid, num)
-    local cords = {}
-    find(grid, num, function(str, first, last)
-        if first-1 >= 1 and last+1 < #content then
-            --print(str:sub(first-1,last+1):match("%d+"),num,str:sub(first-1,last+1):match("%d+")==num)
-            if not(str:sub(first-1,last+1):match("%d+")==num) then
-                --print("RET")
-                return
-            end
-        end
-        x = first % maxX
-        y = math.floor(first/maxX + 1) 
-        table.insert(cords, {x, y, last-first})
-    end)
-    return cords
 end
 
-local function getAdjacentPoints(x, y, len)
-    local adj = {}
+local function isSymbol(data)
+    if data:match("[0-9]") or data=="." or data == "\n" then
+        return false
+    end
+    return true
+end
+
+local function isGear(data)
+    return data == "*"
+end
+
+function tprint (tbl, indent)
+    if not indent then indent = 0 end
+    for k, v in pairs(tbl) do
+      formatting = string.rep("  ", indent) .. k .. ": "
+      if type(v) == "table" then
+        print(formatting)
+        tprint(v, indent+1)
+      elseif type(v) == 'boolean' then
+        print(formatting .. tostring(v))      
+      else
+        print(formatting .. v)
+      end
+    end
+  end
+
+local function smartInsert(tbl, val)
+    for i,v in ipairs(tbl) do
+        if v==val then
+            return false
+        end
+    end
+    table.insert(tbl, val)
+    return true
+end
+
+local gearSum = 0
+--
+local gears = {}
+find(content,"[0-9]+",function(str, first, last)
+    local num = str:sub(first,last)
+    local len = last-first
+    local good = false
+    local isgear = false
+    local gearPos = -1
     for i=-1,len+1 do
-        for v=-1,1 do
-            if y+v >=1 and x+i>=1 and y+v>1 and y+v<=maxY-1 and x+i<=maxX-1 then
-                table.insert(adj,{x+i,y+v})
+        local posPre = (first + i) - maxX
+        local posPost = (first + i) + maxX
+
+        if isSymbol(str:sub(first-1,first-1)) or isSymbol(str:sub(last+1,last+1)) then
+            good = true
+        end
+
+        if isGear(str:sub(first-1,first-1)) then
+            print("Side Gear! First",num,first-1)
+            if not gears[first-1] then
+                gears[first-1] = {num}
+            else
+                smartInsert(gears[first-1], num)
+            end
+            isgear = true
+            gearPos = first-1
+        end
+
+        if isGear(str:sub(last+1,last+1)) then
+            print("Side Gear! Last",num,last-1)
+            if not gears[last+1] then
+                gears[last+1] = {num}
+            else
+                smartInsert(gears[last+1], num)
+            end
+            isgear = true
+            gearPos = last+1
+        end
+
+        if posPre>=1 then
+            local dataPre = str:sub(posPre, posPre)
+            if isSymbol(dataPre) then
+                good = true
+            end
+            if isGear(dataPre) then
+                if not gears[posPre] then
+                    gears[posPre] = {num}
+                else
+                    smartInsert(gears[posPre], num)
+                end
+                isgear = true
+                gearPos = posPre
+            end
+        end
+
+        if posPost<=#content then
+            local dataPost = str:sub(posPost, posPost)
+            if isSymbol(dataPost) then
+                good = true
+            end
+            if isGear(dataPost) then
+                if not gears[posPost] then
+                    gears[posPost] = {num}
+                else
+                    smartInsert(gears[posPost], num)
+                end
+                isgear = true
+                gearPos = posPost
             end
         end
     end
-    return adj
-end
 
-local function getPoint(grid, x, y)
-    first = (y-1) * maxX + x
-    return grid:sub(first,first)
-end
-
-local function getNeighbors(grid, num)
-    local cords = getCords(grid, num)
-    local neighbors = {}
-    for i,v in pairs(cords) do
-        local x, y, len = v[1], v[2], v[3]
-        local points = getAdjacentPoints(x, y, len)
-        for ii,vv in pairs(points) do
-            local sym = getPoint(grid, vv[1],vv[2])
-            if not sym:match("%.") and not sym:match("[0-9]") then
-                -- print(sym, vv[1], vv[2], num)
-                -- print(lines[vv[2]-1])
-                -- print(lines[vv[2]])
-                -- print(lines[vv[2]+1])
-                -- io.read()
-                return true
-            end
-        end
-    end
-    return false
-end
-
-local sum = 0
-
-for num in content:gmatch("[0-9]+") do
-    if getNeighbors((content:gsub("\n","")), num) then
+    if good then
+        print(num,isgear,gearPos)
         sum = sum + tonumber(num)
     end
+end)
+for i,v in pairs(gears) do
+    if #v == 2 then
+        gearSum = gearSum + v[1]*v[2]
+    end
 end
-print(sum)
+print("Sum: "..sum)
+print("GearSum: "..gearSum)
